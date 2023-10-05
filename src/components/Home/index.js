646be03455f9f6fac6129c76e070a935bc5a7b8b
Faith <fthmorgan4@gmail.com>
2023-10-05 12:13:57 -0600

    commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import LogoTitle from '../../assets/images/logo-s.png'
+import LogoTitle from '../../assets/images/logo-f.png'
 import './index.scss'
 import { useState } from "react"
 
@@ -11,9 +11,9 @@ const Home = () => {
     <div className="container home-page">
       <div className="text-zone">
         <h1>Hi, <br />I'm
-          <img src={LogoTitle} alt="developer" />lobodan
+          <img src={LogoTitle} alt="developer" />aith
           <br />
-          web developer
+          a web developer
         </h1>
         <h2>Full-Stack Developer/ Graphic Designer / STEM Enthusiast</h2>
         <Link to="/contact" className="flat-button">CONTACT ME</Link>
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,6 @@
 import { Link, NavLink } from "react-router-dom"
 import './index.scss'
-import LogoS from "../../assets/images/logo-s.png";
+import LogoS from "../../assets/images/logo-f.png";
 import LogoSubtitle from "../../assets/images/logo_sub.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons"
@@ -11,7 +11,7 @@ const Sidebar = () => (
   <div className='nav-bar'>
     <Link className='logo' to='/'>
       <img src={LogoS} alt="logo" />
-      <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+      <img className="sub-logo" src={LogoSubtitle} alt="faith" />
     </Link>
     <nav>
       <NavLink exact="true" activeclassname="active" to="/">
