69d772d21a089466cae0375b86ca33acac8edd47
Faith <fthmorgan4@gmail.com>
2023-10-11 14:13:49 -0600

    commit

---
+++ src/components/Home/Logo/index.js
@@ -0,0 +1,69 @@
+import { useEffect, useRef } from 'react'
+import gsap from 'gsap-trial'
+import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import LogoS from '../../../assets/images/logo-f.png'
+import './index.scss'
+
+const Logo = () => {
+  const bgRef = useRef()
+  const outlineLogoRef = useRef()
+  const solidLogoRef = useRef()
+
+  useEffect(() => {
+    gsap.registerPlugin(DrawSVGPlugin)
+
+    gsap
+      .timeline()
+      .to(bgRef.current, {
+        duration: 1,
+        opacity: 1,
+      })
+      .from(outlineLogoRef.current, {
+        drawSVG: 1,
+        duration: 20,
+      })
+
+    gsap.fromTo(
+      solidLogoRef.current,
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        delay: 4,
+        duration: 4,
+      }
+    )
+  }, [])
+
+  return (
+    <div className="logo-container" ref={bgRef}>
+      <img
+        className="solid-logo"
+        ref={solidLogoRef}
+        src={LogoS}
+        alt="JavaScript,  Developer"
+      />
+
+      <svg
+        width="559pt"
+        height="897pt"
+        version="1.0"
+        viewBox="0 0 559 897"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g
+          className="svg-container"
+          transform="translate(0 457) scale(.1 -.1)"
+          fill="none"
+        >
+          <path
+            d="M6 3H3V6V878V881H6H158.322H161.322V878V510.924H506.075H509.075V507.924V376.076V373.076H506.075H161.322V140.849H542H545V137.849V6V3H542H6Z" stroke="black" stroke-width="6"
+          />
+        </g>
+      </svg>
+    </div>
+  )
+}
+
+export default Logo
